172613c27a0757d309fb3eaf7d864d8c749c2485
Shriyansh p <shriyansh.p44@gmail.com>
2024-03-15 21:57:20 +0530

    data valid

--- src/text_summarizer/entity/__init__.py
+++ src/text_summarizer/entity/__init__.py
@@ -8,3 +8,9 @@ class DataIngestionConfig:
     local_data_file:Path
     unzip_dir:Path
     # return types of function entities
+
+@dataclass(frozen=True)
+class DataValidationConfig:
+    root_dir: Path
+    STATUS_FILE: str
+    ALL_REQUIRED_FILES: list
